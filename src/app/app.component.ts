6bd3f75b3dfb201d5704eb6342798f3785b6793b
bhavyahooptie <bhavya.hooptie@gmail.com>
2022-02-18 00:21:49 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,72 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'rock-paper-scissors';
+  title: string = 'Rock Paper Scissor';
+  playerOne: string = '';
+  playerTwo: string = 'Computer';
+  nameWarning: boolean = false;
+  gameScreenDisplay: boolean = false;
+  playerOneScore: number = 0;
+  playerTwoScore: number = 0;
+  playerOneSelection!: string;
+  playerTwoSelection!: string;
+  result!: string;
+
+  playWithComputer() {
+    if (!this.playerOne) {
+      this.nameWarning = true;
+      console.log(name);
+    } else {
+      this.nameWarning = false;
+      this.gameScreenDisplay = true;
+    }
+  }
+
+  pick(selectedOption: string) {
+    this.playerOneSelection = selectedOption;
+    this.computersPick();
+    this.winner();
+    console.log(this.playerOneSelection);
+    console.log(this.playerTwoSelection);
+  }
+
+  computersPick() {
+    const gameOptions = ['rock', 'paper', 'scissor'];
+    const randomSelection = Math.floor(Math.random() * 3);
+    this.playerTwoSelection = gameOptions[randomSelection];
+  }
+
+  winner() {
+    // if (this.playerOneSelection === this.playerTwoSelection) {
+    //   this.result = "It's a Tie";
+    // } else if ((this.playerOneSelection === 'rock' && this.playerTwoSelection === 'scissor')
+    // || (this.playerOneSelection === 'paper' && this.playerTwoSelection === 'rock')
+    // || (this.playerOneSelection === 'scissor' && this.playerTwoSelection === 'paper')) {
+    //   this.result = "You Won";
+    //   this.playerOneScore++;
+    // } else if ((this.playerOneSelection === 'rock' && this.playerTwoSelection === 'paper')
+    // || (this.playerOneSelection === 'paper' && this.playerTwoSelection === 'scissor')
+    // || (this.playerOneSelection === 'scissor' && this.playerTwoSelection === 'rock')) {
+    //   this.result = "Computer Won";
+    //   this.playerTwoScore++;
+    // } 
+    switch(this.playerOneSelection+this.playerTwoSelection) {
+      case 'rockscissor':
+      case 'paperrock':
+      case 'scissorpaper':
+        this.result = "You Won";
+        this.playerOneScore++;
+        break;
+      case 'rockpaper':
+      case 'scissorrock':
+      case 'paperscissor':
+        this.result = "Computer Won";
+        this.playerTwoScore++;
+        break;
+      default:
+        this.result = "It's a Tie";
+        break;
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatCardModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
